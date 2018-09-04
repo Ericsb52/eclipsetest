99b77b7c451344878bc5c03ac1df6dd0c3cb8937
ebroadbent <ebroadbent@CLC-RM209-TWS.TCSD.tooelesd.org>
2018-09-04 13:18:52 -0600

    first commit

---
+++ src/Ericsb52/helloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * hello world program print hello world in console
+ */
+package Ericsb52.helloWorld;
+
+/**
+ * @author ebroadbent
+ * 9/18
+ * hello world
+ *
+ */
+public class HelloWorld {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+	}
+
+}
